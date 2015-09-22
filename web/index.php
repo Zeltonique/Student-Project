83e0aa12fb18e25a435b707664fd671dadd7cb1b
Zeltonique Damon <zeltonique@outlook.com>
2015-09-22 22:46:36 +0200

    Heroku files added

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Zeltonique's Website</title>
+    </head>
+    <body>
+        <?php
+            $welcome="Helloo and welcome to Zeltonique's Website";
+            echo $welcome;
+        ?>
+    </body>
+</html>
